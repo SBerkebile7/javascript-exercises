4ffd21cb9ce44824269fae76e5b8579512e65fb2
Spencer Berkebile <sberkebile@live.com>
2023-09-15 16:15:18 -0400

    working on tests assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
 const repeatString = function() {
 
+    let loopAmount = 1;
+    let sayHey = "";
+
+    for(let i = 0; i < loopAmount; i++) {
+        sayHey = sayHey + "hey";
+    }
+
+    return sayHey;
 };
 
 // Do not edit below this line
