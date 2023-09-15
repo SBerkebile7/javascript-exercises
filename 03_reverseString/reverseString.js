1b499da5a7771a4cef6e26118931d775fa2de77b
Spencer Berkebile <sberkebile@live.com>
2023-09-15 16:41:35 -0400

    assignment 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    var splitString = word.split("");
 
+    var reverseString = splitString.reverse();
+
+    var joinString = reverseString.join("");
+
+    return joinString;
 };
 
 // Do not edit below this line
