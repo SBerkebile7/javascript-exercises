d38b4dba8fc216a7d230ef8081051dbb3762e807
Spencer Berkebile <sberkebile@live.com>
2023-09-18 16:43:56 -0400

    first test worrkin a4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(x, y) {
+    let fullArray = x;
+    
+    let removeItem = fullArray.indexOf(y);
 
+    fullArray.splice(removeItem, 1);
+
+    console.log(fullArray);
+
+    return fullArray;
 };
 
 // Do not edit below this line
