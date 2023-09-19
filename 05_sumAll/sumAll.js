bdfde28979e84f69402b378633cc4e7011357a80
Spencer Berkebile <sberkebile@live.com>
2023-09-19 16:25:24 -0400

    tests 1 - 6 working on a5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,26 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
 
-    if(num1 < num2) {
-        for(let i = 1; i <= num2; i++) {
-            sum = i + sum;
+    if(num1 > 0 && num2 > 0) {
+        if(Number.isInteger(num1) && Number.isInteger(num2)) {
+            if(num1 < num2) {
+                for(let i = 1; i <= num2; i++) {
+                    sum = i + sum;
+                }
+            } else if(num1 > num2) {
+                for(let i = 1; i <= num1; i++) {
+                    sum = i + sum;
+                }
+            } else {
+                return 'ERROR';
+            }
+        } else {
+            return 'ERROR';
         }
-    } else if(num1 > num2) {
-        for(let i = 1; i <= num1; i++) {
-            sum = i + sum;
-        }
-    } else {
+    }  else {
         return 'ERROR';
     }
+    
 
     return sum;
 };
