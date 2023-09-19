485a7aba1d8b049cb19dc02e007500289721daf5
Spencer Berkebile <sberkebile@live.com>
2023-09-19 17:14:01 -0400

    tests 1 & 2 working a6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = true;
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
+        isLeapYear = true;
+    }
+    else {
+        isLeapYear = false;
+    }
 
+    console.log("year % 4 == 0 actually equals = " + year % 4)
+    console.log("year % 100 != 0 actually equals = " + year % 100)
+    console.log("year % 400 == 0 actually equals = " + year % 400)
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
