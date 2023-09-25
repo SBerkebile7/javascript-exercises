b8e10d86267055461b23f069fb52003ed17a64b8
Spencer Berkebile <sberkebile@live.com>
2023-09-25 16:40:37 -0400

    4 tests working on a7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // (x − 32) * 5/9
+
+  let celConvert = (temp - 32) * (5/9)
+
+  console.log(celConvert);
+
+  celConvert = Math.round(celConvert * 10) / 10;
+
+  return celConvert;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // (x * 9/5 + 32)
+
+  let fahrConvert = (temp * (9/5) + 32);
+
+  console.log(fahrConvert);
+
+  fahrConvert = Math.round(fahrConvert * 10) / 10;
+
+  console.log(fahrConvert);
+
+  return fahrConvert;
 };
 
 // Do not edit below this line
