ffb85748647586cfe582a154437802cf71a8bcf1
Spencer Berkebile <sberkebile@live.com>
2025-11-14 22:31:47 -0800

    first test working for 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  convTempC = (tempF - 32) / (9/5);
+
+  console.log(convTempC);
+
+  let finalTempC = parseFloat(convTempC.toFixed(1)); 
+
+  return finalTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  convTempF = tempC * (9/5) + 32;
+
+  let finalTempF = parseFloat(convTempF.toFixed(1)); 
+
+  return finalTempF;
 };
 
 // Do not edit below this line
