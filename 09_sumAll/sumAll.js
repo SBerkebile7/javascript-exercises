844cd51c775a23721d5d68bb13efe61268f65691
Spencer Berkebile <sberkebile@live.com>
2025-11-14 21:39:58 -0800

    09 work done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if(min > max){
+        const tempMax = min;
+        min =  max;
+        max = tempMax;
+    }
 
+    if(min < 0 || max < 0){ 
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    for(let i = min; i <= max; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
