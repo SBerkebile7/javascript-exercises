b274863128ee6f5fa0fbe703b6d36e0bb152ddd5
Spencer Berkebile <sberkebile@live.com>
2025-11-14 17:11:16 -0800

    test push

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    let str = "";
+    for(let i = 0; i < times; i++){
+        str+=word;
+    }
+    return str;
 };
 
 // Do not edit below this line
