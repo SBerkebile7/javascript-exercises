67e96083da61c63efbe5fb136a77f0213564292c
Spencer Berkebile <sberkebile@live.com>
2025-11-14 20:19:50 -0800

    test push

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,10 @@
-const repeatString = function(word, count) {
-    let str = "";
-    for(let i = 0; i < times; i++){
-        str+=word;
-    }
-    return str;
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
