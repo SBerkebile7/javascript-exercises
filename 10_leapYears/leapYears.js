45fe2fd759af9db0e43aff54bd6d54b919ef1f9f
Spencer Berkebile <sberkebile@live.com>
2025-11-14 21:48:58 -0800

    test work on assignment 10, will continue with tests in morning

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearDivFour = year % 4 === 0;
+    const yearNotCentury = year % 100 === 0;
+    const yearFourHundred = year % 400 === 0;
 
+    if (yearDivFour && (!yearNotCentury || yearFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
