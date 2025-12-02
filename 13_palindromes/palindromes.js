407da84e0074af89f984e13dde8f470224364e7a
Spencer Berkebile <sberkebile@live.com>
2025-12-02 12:19:03 -0800

    tests 1 & 2 working

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
 const palindromes = function (string) {
- return string == string.split('').reverse('').join('');
+
+    const alpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const updatedString = string
+                        .toLowerCase('')
+                        .split('')
+                        .filter((character) => alpha.includes(character))
+                        .join('');
+
+    const reverseString = updatedString
+                        .split('')
+                        .reverse('')
+                        .join('');
+
+    return updatedString === reverseString;
 };
 
 // Do not edit below this line
