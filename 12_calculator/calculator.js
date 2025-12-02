54f935a35c4c63f69944dd35165bd33a9c61473a
Spencer Berkebile <sberkebile@live.com>
2025-12-02 11:51:07 -0800

    tests working for all but factorial

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	 return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((x, y) => x + y, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((x, y) => x * y)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factor = 1;
+  for(let i = n; i > 0; i--) {
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
