df9778cff321b03b84f834e5e00a1d663a521ded
Spencer Berkebile <sberkebile@live.com>
2025-12-02 14:35:48 -0800

    getTheOldest functioning, all tests pass

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getDeath = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return (death - birth);
+};
 
+const findTheOldest = function(people) {
+    return people.reduce(function (a, b) {
+        const firstPerson = getDeath(a.yearOfBirth, a.yearOfDeath);
+        const secondPerson = getDeath(b.yearOfBirth, b.yearOfDeath);
+
+        return firstPerson < secondPerson ? b : a;
+    });
 };
 
 // Do not edit below this line
