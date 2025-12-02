41f73604992393736697f4ac249a5dc1d4504474
Spencer Berkebile <sberkebile@live.com>
2025-12-02 14:00:37 -0800

    all tests functioning for fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    let n
+    if(typeof fib !== 'number') {
+        n = parseInt(fib);
+    } else {
+        n = fib;
+    }
+    
 
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let x = 1;
+    let y = 0;
+
+    for(let i = 2; i <= n; i++) {
+        let temp = x + y;
+        y = x;
+        x = temp;
+    };
+    return x;
 };
 
 // Do not edit below this line
